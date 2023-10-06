994e35a8b20453198897d8bc1266295a46f9ee80
Leonardo <leonardoserzedello@hotmail.com>
2023-10-06 12:19:55 +0100

    Update-ortográfica
    correção ortográfica

--- Escola/Paginas/generos/ver.php
+++ Escola/Paginas/generos/ver.php
@@ -12,16 +12,16 @@ $genero = obterGenero($ligacao, $id);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detalhes do Genero literario <?php echo $genero["genero"]?></title>
+    <title>Detalhes do Genero  <?php echo $genero["genero"]?></title>
 </head>
 <body>
 <?php  include "../../Componentes/nav.php"?>
-        <h1>Detalhes de Genero literario <?php echo $genero["genero"];?> </h1>
+        <h1>Detalhes de Genero  <?php echo $genero["genero"];?> </h1>
         <p>
             <label> ID: </label><?php echo $genero['id'];?> <br>
         </p>
         <p>
-            <label> Tema: </label><?php echo $genero['genero']; ?><br>
+            <label> Genero: </label><?php echo $genero['genero']; ?><br>
         </p>
 </body>
 </html>
